5fc27e8d7a0338848562cfc7895e429d92d11924
Lucas Romagnoli <95435728+LucasCART@users.noreply.github.com>
2024-10-29 11:39:56 -0700

    Added imports "SimpleDateFormat" and "java.util.Date"

--- Oct22Hyena.java
+++ Oct22Hyena.java
@@ -1,5 +1,8 @@
 package lucas.zoo.com;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Hyena extends lucas.zoo.com.Animal {
     // Create a static int that keeps track of the number of animals created.
     static int numOfHyenas = 0;
